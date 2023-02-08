938d5540f12f9fa85ebdf303dde7918464bc7897
Caleb Ely <le717@users.noreply.github.com>
2023-02-07 22:55:44 -0500

    Add ability to specify date format

--- generate.py
+++ generate.py
@@ -27,10 +27,19 @@ def get_meta(content: str, /) -> dict:
 
 
 def get_config() -> dict[str, Path]:
+    """Get the config JSON for the generator."""
     config = json.loads(Path("config.json").read_text())
-    config = {k: Path(v) for k, v in config.items()}
+    not_paths = ["date_format"]
+    config = {k: Path(v) if k not in not_paths else v for k, v in config.items()}
     config["note_path"] = config["output"] / config["note_path"]
     return config
+
+
+def _format_dt(dt: datetime, fmt: str) -> str:
+    """Format a datetime object to a  datestring."""
+    if fmt == "iso":
+        return dt.isoformat()
+    return dt.strftime(fmt)
 
 
 def main():
@@ -47,22 +56,24 @@ def main():
     # Generate each note
     all_notes = []
     for f in config["input"].iterdir():
-        # Get the note content
+        # Get the note content and metadata
         content = f.read_text()
-
-        # Get the note metadata
         meta = get_meta(content)
 
-        # Remove the raw meta from the text
+        # Remove the raw meta from the note
         content = content.replace(meta["raw_text"], "").strip()
 
-        # Convert the publish date to a datetime object for further processing
+        # Convert the publish date to a datetime object for better usage
         date = datetime.fromisoformat(meta["date"])
 
         # Fill in all the content
         content = re.sub(r"<!--content-->", content, template_note)
         content = re.sub(r"<!--title-->", meta["title"], content)
-        content = re.sub(r"<!--date-->", date.isoformat(), content)
+        content = re.sub(
+            r"<!--date-->",
+            _format_dt(date, config["date_format"]),
+            content,
+        )
 
         # Keep a record of the note so we can generate the index when we are done
         note_file = f"{quote_plus(meta['url'])}.html"
@@ -78,7 +89,13 @@ def main():
     html = []
     for note in all_notes:
         html.append("<li>")
-        html.append('<a href="note/{url}">{title} ({date})</a>'.format_map(note))
+        html.append(
+            '<a href="note/{url}">{title} ({date})</a>'.format(
+                url=note["url"],
+                title=note["title"],
+                date=_format_dt(note["date"], config["date_format"]),
+            )
+        )
         html.append("</li>")
     html = "".join(html)
 
