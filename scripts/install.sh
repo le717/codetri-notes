9aadf5b5ad336259d0ac6fc56bd8a8545ffe31ef
Caleb Ely <le717@users.noreply.github.com>
2024-10-22 21:18:53 -0400

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Make sure the virtualenv package is installed
+apt install python3-venv -y
+
+# Create the virutalenv
+python3 -m venv venv
+
+# Install the app deps
+venv/bin/python -m pip install pip --upgrade
+venv/bin/python ./scripts/get-requirements.py
+venv/bin/pip install -r ./requirements.txt
