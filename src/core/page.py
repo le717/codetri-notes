6362e294dae100d29dfa54a8fcace644ed0bc0ae
Caleb Ely <le717@users.noreply.github.com>
2024-11-07 22:20:19 -0500

    remove old meta parser

--- src/core/page.py
+++ src/core/page.py
@@ -1,29 +1,10 @@
-from configparser import ConfigParser
 from pathlib import Path
 
 import minify_html
 from jinja2 import Environment
 
 
-__all__ = ["meta", "render", "replace_curly_quotes", "write"]
-
-
-def meta(content: str, /) -> dict[str, str]:
-    """Extract a note's metadata."""
-    # Use the Python `configparser` for quickness for note metadata
-    parser = ConfigParser(default_section="meta")
-
-    # Get the metadata
-    start_tag = "[meta]"
-    end_tag = "[endmeta]"
-    raw_text = content[content.find(start_tag) : content.find(end_tag) + len(end_tag)]
-    parser.read_string(raw_text)
-
-    # Pull the metadata out of the base key and append the raw text
-    note_meta = parser["meta"]
-    note_meta["raw_text"] = raw_text
-    parser.clear()
-    return note_meta
+__all__ = ["render", "replace_curly_quotes", "write"]
 
 
 def render(template: str, render_opts: dict, jinja: Environment) -> str:
